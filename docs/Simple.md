677c99b9f27707b12bb32edaf99e1f0ea93edf06
Adam Shannon <adam@ashannon.us>
2015-06-09 19:40:13 -0500

    Removing all the old, I'm sure i'll be useful at some point though.

--- docs/Simple.md
+++
@@ -1,7 +0,0 @@
-# Simple Yeboo examples
-
-## Hello, World!
-
-Something like the following
-
-    1 (1g) (2b) (1x)
--- scala/src/main/scala/pre-processing/PreProcessor.scala
+++
@@ -1,4 +0,0 @@
-package yeboo
-
-// Run everything here before parser and image generation.
-// Gives us nice layers above everything else.
